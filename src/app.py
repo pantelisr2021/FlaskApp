c6dde5f8542a3ddfb93c304e41255cb01d096763
Pantelis R <pantelisr2021@gmail.com>
2023-01-20 12:07:32 +0200

    opwet1

--- src/app.py
+++ src/app.py
@@ -32,10 +32,6 @@ def new_article():
 
 @app.route("/okairos/")
 def okairos():
-    url=https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}.format(os.environ.get("TMDB_API_KEY"))
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
     return render_template("okairos.html")
 
 
