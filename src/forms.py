c3f341e55db70696003891f40a2dc67a596cba6c
Pantelis R <pantelisr2021@gmail.com>
2023-02-10 12:11:17 +0200

    val3

--- src/forms.py
+++ src/forms.py
@@ -4,20 +4,24 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class SignupForm(FlaskForm):
     username = StringField(label="Username",
-                            validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"),
-                                        Length(min=3, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 εως 15 χαρακτήρες")])
+                        validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"),
+                                    Length(min=5, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 εως 15 χαρακτήρες")])
 
     email = StringField(label="Email",
                         validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"),
-                                    Email(message = "Παρακαλώ εισάγετε σωστά το εμαιλ ") ])
+                                    Email(message = "Παρακαλώ εισάγετε σωστά το εμαιλ")])
 
 
     password=StringField(label="Password",
-                        validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό")])
+                        validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"),
+                                    Length(min=5, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 εως 15 χαρακτήρες")])
+                        
+            
 
     password2=StringField(label="Επιβεβαίωση password",
                           validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να είναι κενό"),
-                                     EqualTo("password")])
+                                     EqualTo("password")
+                                     Length(min=5, max=15, message="Αυτό το πεδίο πρέπει να είναι από 3 εως 15 χαρακτήρες")])
 
     submit=SubmitField("Εγγραφή") 
 
